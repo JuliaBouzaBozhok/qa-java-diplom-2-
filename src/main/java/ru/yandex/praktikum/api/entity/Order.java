1074cef2058a3f90d934e94e324aec7dd358c4f5
Julia Bouza <julia.jmpr@yandex.ru>
2024-03-10 23:08:23 +0300

    added tests, created new branch

--- src/main/java/ru/yandex/praktikum/api/entity/Order.java
+++ src/main/java/ru/yandex/praktikum/api/entity/Order.java
@@ -1,4 +1,21 @@
 package ru.yandex.praktikum.api.entity;
 
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class Order {
+
+    private List<String> ingredients;
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public Order() {
+        ingredients = new ArrayList<>();
+    }
 }
